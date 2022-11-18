a52bf790c47e499c33a4b98dbe2d4834ef22c4bf
cristianrz155 <cristianstivenrz@gmail.com>
2022-11-18 11:18:13 -0500

    probando modificaciones

--- js/media-query.js
+++ js/media-query.js
@@ -1,8 +1,20 @@
-/* alert("mensaje  javascript!") */
+/* alert("deseas ver la pagina ") */
 
-let barmenu=document.getElementById("barmenu");
+let barmenu = document.getElementById("barmenu");
+let main_menu = document.getElementById("mainmenu");
 
 barmenu.addEventListener("click",function(){
+    if (main_menu.classList.contains("main-menu")){
+        main_menu.classList.add("href-item");
+        main_menu.classList.remove("main-menu");
+    }
+    else{
+        main_menu.classList.remove("href-item");
+        main_menu.classList.add("main-menu");
+        
+    }
+    
+});
+alert("funciona);"
 
-    alert("FUNCIONA");
-})
+)
